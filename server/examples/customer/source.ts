d38e16009509ab5e0b102f1cd2e6071d09b3825b
it1shka <2tishbel@gmail.com>
2024-05-30 19:38:00 +0200

    Added one more example

---
+++ server/examples/customer/source.ts
@@ -0,0 +1,22 @@
+class Customer {
+  constructor (
+    public readonly name: string,
+    public readonly age: number,
+    public readonly money: number
+  ) {}
+}
+
+class Queue {
+  constructor (
+    public readonly customers: Customer[],
+  ) {}
+}
+
+const queue = new Queue([
+  new Customer('Name', 18, 200),
+  new Customer('', -1, -1)
+])
+
+for (const each of queue.customers) {
+  oclVerify(each)
+}
