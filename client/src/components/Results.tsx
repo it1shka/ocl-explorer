5ebafc9104fd0f3239d3720fbfde2a085bf68623
it1shka <2tishbel@gmail.com>
2024-05-31 19:19:51 +0200

    Added small console

--- client/src/components/Results.tsx
+++ client/src/components/Results.tsx
@@ -1,18 +1,63 @@
-import { memo } from 'react'
+import { memo, useEffect, useRef, useState } from 'react'
 import styled from 'styled-components'
 import Controls from './controls'
-
-// TODO: 
+import { useAppSelector } from '../storage/hooks'
 
 const Results = () => {
+  const { entries, loading } = useAppSelector(({ results }) => results)
+
+  const listRef = useRef<HTMLDivElement>(null)
+  useEffect(() => {
+    if (!listRef.current) return
+    listRef.current.scrollTo({
+      top: listRef.current.scrollHeight,
+      behavior: 'smooth',
+    })
+  }, [entries])
+
   return (
     <Container>
       <Controls />
+      <Console ref={listRef}>
+        <Mock $size={20} />
+        { entries.map((entry, index) => (
+          <Log key={index}>{'> '}{entry}</Log>
+        )) }
+        {loading && <Loading />}
+        <Mock $size={50} />
+      </Console>
     </Container>
   )
 }
 
 export default memo(Results)
+
+const Loading = memo(() => {
+  const [dots, setDots] = useState(0)
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setDots(prev => (prev + 1) % 3)
+    }, 100)
+    return () => clearInterval(interval)
+  }, [])
+  return <Log>Executing{'.'.repeat(dots)}</Log>
+})
+
+const Mock = styled.div<{ $size: number }>`
+  height: ${({ $size }) => $size}px;
+  background-color: transparent;
+`
+
+const Log = styled.p`
+  color: grey;
+  font-size: 1.2em;
+  padding: 0.2em 0.5em;
+`
+
+const Console = styled.div`
+  flex: 1;
+  overflow: scroll;
+`
 
 const Container = styled.div`
   grid-row: 2 / 3;
