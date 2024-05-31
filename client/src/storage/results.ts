744e62230295ed3d18d5fbbfda5a1c97f34ac735
it1shka <2tishbel@gmail.com>
2024-05-31 09:58:17 +0200

    Started doing results

---
+++ client/src/storage/results.ts
@@ -0,0 +1,14 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+// TODO:
+// const verifyCode = createAsyncThunk
+
+const results = createSlice({
+  name: 'results',
+  initialState: new Array<string>(),
+  reducers: {
+    
+  }
+})
+
+export default results.reducer
