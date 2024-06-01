123eea9c228b1edef5988583a0f41e8dba503bac
it1shka <2tishbel@gmail.com>
2024-06-01 15:54:49 +0200

    it kinda works but only with typeName

--- server/src/verify.ts
+++ server/src/verify.ts
@@ -1,7 +1,12 @@
 import type { Request, Response } from 'express'
 import { ScriptTarget, transpile } from 'typescript'
-import { OclEngine  } from '@stekoe/ocl.js'
 import { Isolate } from 'isolated-vm'
+import { OclEngine } from '@stekoe/ocl.js'
+import { rollup } from 'rollup'
+import virtual from '@rollup/plugin-virtual'
+import nodeResolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import json from '@rollup/plugin-json'
 
 const getMessage = (error: unknown) => {
   return error instanceof Error
@@ -20,83 +25,102 @@ const checkBody = (body: any): body is CodeBody => {
     typeof ocl === 'string'
 }
 
-const prepareJS = (code: string) => {
+const prepareSourceCode = (code: string) => {
   try {
     return transpile(code, {
       strict: false,
       allowJs: true,
-      target: ScriptTarget.ES3
+      target: ScriptTarget.ES2019
     })
   } catch (error) {
-    throw new Error(`JS/TS Error: ${getMessage(error)}`)
+    throw new Error(`TS Compiler Error: ${getMessage(error)}`)
   }
 }
 
-const prepareOCLEngine = (code: string) => {
+const verifyOCL = (code: string) => {
   try {
-    const engine = OclEngine.create()
-    engine.addOclExpression(code)
-    return engine
+    OclEngine.create().addOclExpression(code)
   } catch (error) {
     throw new Error(`OCL Error: ${getMessage(error)}`)
   }
 }
 
-type OclResult = ReturnType<OclEngine['evaluate']>
-
-// TODO: IMPORTANT potential hack threat
-// TODO: not working, rebuild
-const runEngineAgainstCode = (engine: OclEngine, code: string) => {
-  try {
-    const results = new Array<OclResult>()
-    const isolate = new Isolate({ 
-      memoryLimit: 8,
-      onCatastrophicError: () => {
-        throw new Error('Fatal crash, most likely due to memory limit')
+const prepareBundle = async (js: string, ocl: string) => {
+  const script = `
+    import { OclEngine } from '@stekoe/ocl.js';
+    const verify = (() => {
+      const engine = OclEngine.create();
+      engine.addOclExpression(\`${ocl}\`);
+      let index = 0;
+      return (obj) => {
+        const result = engine.evaluate(obj);
+        const idx = String(index++);
+        if (result.getResult()) {
+          external.log('Verification #' + idx + ': passed');
+          return;
+        }
+        const failed = result.getNamesOfFailedInvs().join(', ');
+        external.log('Verification #' + idx + ': ' + failed + ' failed');
       }
+    })();
+
+    ${js}
+  `
+  try {
+    const build = await rollup({
+      input: 'script',
+      plugins: [ 
+        nodeResolve(),
+        commonjs(),
+        json(),
+        virtual({ script }),
+      ],
     })
-    const context = isolate.createContextSync()
-    const jail = context.global
-    jail.setSync('OCL', jail.derefInto())
-    jail.setSync('verify', (...args: unknown[]) => {
-      if (args.length <= 0) return
-      const result = engine.evaluate(args[0])
-      results.push(result)
+    const output = await build.generate({
+      format: 'iife'
     })
-    const compiledJS = isolate.compileScriptSync(code)
-    compiledJS.runSync(context)
-    return results
+    return output.output[0].code
   } catch (error) {
-    throw new Error(`Runtime Error: ${getMessage(error)}`)
+    throw new Error(`Bundler Error: ${getMessage(error)}`)
   }
 }
 
-// TODO: maybe add new line
-const resultsToString = (results: OclResult[]) => {
-  return results
-    .map((result, index) => {
-      const failedContexts = result.getNamesOfFailedInvs()
-      if (failedContexts.length <= 0) {
-        return `Verification #${index}: passed`
+const runBundle = async (bundle: string) => {
+  try {
+    const logs = new Array<string>()
+    const logFunction = (...args: unknown[]) => {
+      logs.push(...args.map(String))
+    }
+    const isolate = new Isolate({ 
+      memoryLimit: 8,
+      onCatastrophicError: () => {
+        throw new Error('Fatal crash (most likely due to memory lack)')
       }
-      const failed = failedContexts.join(', ')
-      return `Verification $${index}: failed ${failed}`
     })
-    .join('; ')
+    const ctx = await isolate.createContext()
+    const jail = ctx.global
+    await jail.set('external', jail.derefInto())
+    await jail.set('log', logFunction)
+    const compiledScript = await isolate.compileScript(bundle)
+    await compiledScript.run(ctx)
+    return logs.join('\n')
+  } catch (error) {
+    throw new Error(`VM Error: ${getMessage(error)}`)
+  }
 }
 
-// TODO: 
-const verifyHandler = (req: Request, res: Response) => {
+const verifyHandler = async (req: Request, res: Response) => {
   if (!checkBody(req.body)) {
     res.status(400).send('Expected fields for javascript and ocl code')
     return
   }
   try {
-    const js = prepareJS(req.body.js)
-    const oclEngine = prepareOCLEngine(req.body.ocl)
-    const results = runEngineAgainstCode(oclEngine, js)
-    const response = resultsToString(results)
-    res.send(response)
+    const { js, ocl } = req.body
+    const sourceCode = prepareSourceCode(js)
+    verifyOCL(ocl)
+    const bundle = await prepareBundle(sourceCode, ocl)
+    const result = await runBundle(bundle)
+    res.send(result)
   } catch (error) {
     res.send(getMessage(error))
   }
